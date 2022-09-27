ca9094bdaab58c2756dd1f7128b8f8552c40c345
ASchaffer8770 <aschaffer8770@gmail.com>
2022-09-27 15:16:34 -0700

    getter/setter

---
+++ OOP/BaristaChallenge/Order.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Order {
+
+    //field
+    private String name;
+    private double total;
+    private boolean ready;
+    private ArrayList<Item> items = new ArrayList<Item>();
+}
