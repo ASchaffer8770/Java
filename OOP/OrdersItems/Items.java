5786481f373acb4f46f3286779684d0c8c3f2227
ASchaffer8770 <aschaffer8770@gmail.com>
2022-09-27 07:44:15 -0700

    added constructor

--- OOP/OrdersItems/Items.java
+++ OOP/OrdersItems/Items.java
@@ -1,6 +1,16 @@
 public class Items {
+
+    //fields
     public String name;
     public double total;
     public boolean ready;
     public ArrayList<Item> items;
+
+    //items constructor
+    public Items (String name, double total, boolean ready, ArrayList<Item> items) {
+        this.name = name;
+        this.total = total;
+        this.ready = ready;
+        this.items = items;
+    }
 }
--- OOP/OrdersItems/Orders.java
+++ OOP/OrdersItems/Orders.java
@@ -1,4 +1,12 @@
 public class Orders {
+
+    //field
     public String name;
     public double price;
+
+    //constructor
+    public Orders (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 }
